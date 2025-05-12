af76caf031530bf2002305ea16197359380b24f1
hà hoàng tiến đạt <zhadat1234567@gmail.com>
2025-05-12 23:04:09 +0700

    sửa lỗi connect

--- adminpage/connect.php
+++ adminpage/connect.php
@@ -2,7 +2,7 @@
 $host = "localhost";      
 $username = "root";       
 $password = "";           
-$dbname = "fashionmarket";
+$dbname = "fashionmarket1";
 
 // Tạo kết nối
 $conn = new mysqli($host, $username, $password, $dbname);
